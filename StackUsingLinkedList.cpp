7e12b74ce67cdcbb65babcea474e20e0911a60eb
Kevin Adrian <kevin19305.ib@gmail.com>
2025-05-20 10:02:34 +0700

    tambahkan node *next untuk pointer  ke node berikutnya dalam list

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -4,4 +4,4 @@ using namespace std;
 class Node {
 public:
     int data;                               // Menyimpan nilai data
- 
+    Node *next;                             // Pointer ke node berikutnya dalam list
